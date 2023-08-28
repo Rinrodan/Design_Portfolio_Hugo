3e4b0c772aea122155552f985e923ae8aa441cb9
rinrodan <marioboost@gmail.com>
2023-08-27 22:25:32 -0600

    tried shadui and two components wouldnt work, looking for a new ui

--- astro/tailwind.config.cjs
+++ astro/tailwind.config.cjs
@@ -2,7 +2,31 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		
+		screens: {
+			'sm': '350px',
+			// => @media (min-width: 640px) { ... }
+	  
+			'md': '640px',
+			// => @media (min-width: 768px) { ... }
+	  
+			'lg': '1024px',
+			// => @media (min-width: 1024px) { ... }
+	  
+			'xl': '1280px',
+			// => @media (min-width: 1280px) { ... }
+	  
+			'2xl': '1536px',
+			// => @media (min-width: 1536px) { ... }
+		  
+		},
+		colors: {
+			colorBG: '#050b1a',
+			colorText: '#ffffff',
+			colorAccent: '#5850EC',
+			colorCTA: '#5850EC',
+		}
+		
 	},
 	variants: {
 		extend: {
