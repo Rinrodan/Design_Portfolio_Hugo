47a464bd5f508b1f78e40d11dbe260f4ba086729
coding-admin <mario.bustillos@spaceforce.mil>
2023-10-17 18:01:04 -0600

    carousel javascript works thanks to console log as you go

--- htmlCSSOnly/script.js
+++ htmlCSSOnly/script.js
@@ -51,31 +51,187 @@ function loadHTML(){
     .then(response=> response.text())
     .then(text=> document.getElementById('about').innerHTML = text);
   } getAbout()
-  function getGalleryCarousel(){
-    fetch('./components/galleryCarousel.html')
-    .then(response=> response.text())
-    .then(text=> document.getElementById('galleryCarouselContainer').innerHTML = text);
-  } getGalleryCarousel()
+
 
   
-  const mendu = document.getElementById("mendu");
+  function smallImageOneClick(){
+    console.log("small image one click")
+            document.getElementById('large-image-1').classList.toggle('visibility')
+            document.querySelector('.large-image').classList.toggle('scaled-up');
+            document.querySelector('.large-image').classList.toggle('moved-up');
+        };
+// ================================================== carousel ===================
 
-  const toggleMendu = () => {
-    console.log(mendu);
-    if (mendu !== null) {
-      mendu.classList.toggle("show");
-  }}
+function carouselLinkOne(){
+  console.log("carousel Link One click")
+          // document.getElementById('large-image-1').classList.toggle('visibility')
+          // document.querySelector('.large-image').classList.toggle('scaled-up');
+          // document.querySelector('.large-image').classList.toggle('moved-up');
+};
+  // function carousel(){
+  //   let carouselNavLinks = document.querySelectorAll(".carouselNavLink");
+  //   let slides = document.querySelectorAll(".slideImage");
+  //   let overlays = document.querySelectorAll(".bar");
+  //   let maxZIndex = carouselNavLinks.length;
+  //   let easeInOutQuart = "cubic-bezier(0.77, 0, 0.175, 1)";
+  //   slides[0].classList.add("active");
+  //   carouselNavLinks[0].classList.add("active");
+  //   console.log("slides")
+    
+    
+  //   carouselNavLinks.forEach((carouselNavLink, activeIndex) => {
+  //       overlays[activeIndex].style.zIndex = `${carouselNavLinks.length - activeIndex}`;
+    
+  //       carouselNavLink.addEventListener("click", () => {
+    
+  //         consle.log("asdfgasdfadsfadsf");
+  //           carouselNavLinks.forEach((link) => link.classList.remove("active"));
+  //           carouselNavLink.classList.add("active");
+    
+  //           slides.forEach((slide) => slide.classList.remove("active"));
+  //           slides[activeIndex].classList.add("active");
+    
+  //           overlays.forEach((overlay) => overlay.style.zIndex = "0");
+  //           overlays[activeIndex].style.zIndex = `${maxZIndex++}`;
+    
+  //           overlays.forEach((overlay) => {
+  //               overlay.animate(
+  //                   [{ transform: "scaleX(0)" }, { transform: "scaleX(1)" }],
+  //                   { duration: 1200, fill: "forwards", easing: easeInOutQuart }
+  //               );
+  //           });
+  //       });
+  //   });
+  // };
 
-  window.onclick = (event) => {
-    if (!event.target.matches('.btn')) {
-      if(mendu !== null && mendu.classList.contains('show')){
-          mendu.classList.remove('show')
-      }
-  }
-}
+
+  function logInsideCarousel() {
+    console.log("carouselLinks")
+   }
+  function carouselScript() {
+      let carouselNavLinks = [];
+      let slides = [];
+      let overlays = [];
+      let slideComments = [];
+      let maxZIndex = {};
+      let easeInOutQuart = "cubic-bezier(0.77, 0, 0.175, 1)";
+
+
+    setTimeout(function() {
+      const nodeListLinks = document.querySelectorAll('.carouselNavLink');
+      const nodeArrayLinks = Array.from(nodeListLinks);
+      const nodeListSlides = document.querySelectorAll(".slideImage");
+      const nodeArraySlides = Array.from(nodeListSlides);
+      const nodeListOverlays = document.querySelectorAll(".bar");
+      const nodeArrayOverlays = Array.from(nodeListOverlays);
+      const nodeListComments = document.querySelectorAll(".slideComment");
+      const nodeArrayComments = Array.from(nodeListComments);
+      
+      carouselNavLinks.push(nodeArrayLinks);
+      slides.push(nodeArraySlides);
+      overlays.push(nodeArrayOverlays);
+      slideComments.push(nodeArrayComments);
+
+      const nodeListMaxZ = carouselNavLinks[0].length;
+      maxZIndex = nodeListMaxZ;
+      carouselNavLinks[0][0].classList.add("active");
+      slides[0][0].classList.add("active");
+      slideComments[0][0].classList.add("active");
+      console.log("overlays after update",overlays);
+      console.log("variables after update", carouselNavLinks, slides, overlays, maxZIndex);
+      console.log("comments after update", slideComments[0]);
+    }, 300);
+
+    setTimeout(function() {
+      carouselNavLinks[0].forEach((carouselNavLink, activeIndex) => {
+        console.log("setting activeindex", carouselNavLinks[0])
+        console.log("setting activeindex - overlays", overlays);
+        console.log("setting activeindex - comments", slideComments[0]);
+            overlays[0][activeIndex].style.zIndex = `${maxZIndex - activeIndex}`;
+        
+            carouselNavLink.addEventListener("click", () => {
+        
+              console.log("asdfgasdfadsfadsf");
+                carouselNavLinks[0].forEach((link) => link.classList.remove("active"));
+                carouselNavLink.classList.add("active");
+
+
+        
+                slides[0].forEach((slide) => slide.classList.remove("active"));
+                slides[0][activeIndex].classList.add("active");
+        
+                overlays[0].forEach((overlay) => overlay.style.zIndex = "0");
+                overlays[0][activeIndex].style.zIndex = `${maxZIndex++}`;
+        
+                overlays[0].forEach((overlay) => {
+                    overlay.animate(
+                        [{ transform: "scaleX(0)" }, { transform: "scaleX(1)" }],
+                        { duration: 1200, fill: "forwards", easing: easeInOutQuart }
+                    );
+                });
+                slideComments[0].forEach((slideComment) => slideComment.classList.remove("active"));
+                slideComments[0][activeIndex].classList.add("active");
+
+              slideComments[0].forEach((comment) => {
+                  comment.animate(
+                      [{ transform: "scaleX(0)" }, { transform: "scaleX(1)" }],
+                      { duration: 1200, fill: "forwards", easing: easeInOutQuart }
+                  );
+              });
+            });
+        });
+    },350);
+  };
+    // let carouselNavLinks = document.querySelectorAll("carouselNavLink");
+ 
+// const handleCloseCarouselSlide = () => {
+//    addEventListener("click", () => {
+    
+//   console.log(carouselLinks);
+//     carouselNavLinks.forEach((link) => link.classList.remove("active"));
+//     carouselNavLink.classList.add("active");
+
+//     slides.forEach((slide) => slide.classList.remove("active"));;
+// })};
+
+function carouselLinkOne(){
+  console.log("carousel Link One click")
+          document.getElementById('carouselSlideOne').classList.toggle('active')
+          // document.querySelector('.large-image').classList.toggle('scaled-up');
+          // document.querySelector('.large-image').classList.toggle('moved-up');
+};
+
+
+
+  // ================================================== carousel ===================
+          // function getGalleryCarousel(){
+  //   fetch('./components/galleryCarousel.html')
+  //   .then(response=> response.text())
+  //   .then(text=> document.getElementById('galleryCarouselContainer').innerHTML = text);
+  // } getGalleryCarousel()
+
+//   window.addEventListener("load", ()=>{
+//     const mendu = document.getElementById("#mendu");
+//     mendu.addEventListener("click", handler);
+// });
+
+//   const toggleMendu = () => {
+//     console.log(mendu);
+//     if (mendu !== null) {
+//       mendu.classList.toggle(".show");
+//   }}
+
+//   window.onclick = (event) => {
+//     if (!event.target.matches('btn')) {
+//       if(mendu !== null && mendu.classList.contains('.show')){
+//           mendu.classList.remove('.show')
+//       }
+//   }
+// }
   
   
-  mendu.addEventListener('click', (event) => event.stopPropagation());
+
+
   
 //   function getHeaderContent(){
 //     fetch('./components/header.html')
@@ -84,37 +240,4 @@ function loadHTML(){
 //   } getHeaderContent()
 
 //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
- // carousel  ========================================
-//  let carouselNavLinks = document.querySelectorAll(".carouselNavLink");
-//  let slides = document.querySelectorAll(".slideImage");
-//  let overlays = document.querySelectorAll(".bar");
-//  let maxZIndex = carouselNavLinks.length;
-//  let easeInOutQuart = "cubic-bezier(0.77, 0, 0.175, 1)";
-//  slides[0].classList.add("active");
-//  carouselNavLinks[0].classList.add("active");
-
-//  consle.table(carouselNavLinks);
-
-//  carouselNavLinks.forEach((carouselNavLink, activeIndex) => {
-//      overlays[activeIndex].style.zIndex = `${carouselNavLinks.length - activeIndex}`;
-
-//      carouselNavLink.addEventListener("click", () => {
-
-
-//          carouselNavLinks.forEach((link) => link.classList.remove("active"));
-//          carouselNavLink.classList.add("active");
-
-//          slides.forEach((slide) => slide.classList.remove("active"));
-//          slides[activeIndex].classList.add("active");
-
-//          overlays.forEach((overlay) => overlay.style.zIndex = "0");
-//          overlays[activeIndex].style.zIndex = `${maxZIndex++}`;
-
-//          overlays.forEach((overlay) => {
-//              overlay.animate(
-//                  [{ transform: "scaleX(0)" }, { transform: "scaleX(1)" }],
-//                  { duration: 1200, fill: "forwards", easing: easeInOutQuart }
-//              );
-//          });
-//      });
-//  });
+ 
